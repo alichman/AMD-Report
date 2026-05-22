989bd6a3397ca2d6a21e2bafc4d88540ec0ecfaa
alichman <andrewlichmanov@gmail.com>
2026-05-22 17:56:12 -0400

    role text

---
+++ amd-report/src/Text/role.ts
@@ -0,0 +1,8 @@
+export const role:string = `
+As an associate software engineer, my role from the start was to investigate and fix bugs in the Display Abstraction Layer.
+
+This often involved putting together reproducing systems, regression testing, speaking with developers and testers across the world in order to determine the issues, and a lot of exploring the driver codebase to get to the bottom of the issue.
+Additionally, I was occasionally responsible for minor migrations, regression fixes, and even introducing some power saving features to a new platform.
+
+I often found myself aiding remote senior engineers in putting together testing systems as well, getting the chance to work with a variety of unique hardware.
+`.trim();
--- amd-report/src/components/Floater.tsx
+++ amd-report/src/components/Floater.tsx
@@ -3,15 +3,22 @@ import "../App.css";
 import './Floater.css';
 import {goals} from "../Text/goals";
 import {useMemo} from "react";
+import {role} from "../Text/role";
 
 const GoalsContent = () => {
     return <div className="hScroll">
         <div>
-        {goals.map(g => <div className="vContent">
+        {goals.map(g => <div className="vContent" style={{width:400}}>
             <h3>{g.title}</h3>
             <p>{g.content}</p>
         </div>)}
         </div>
+    </div>
+}
+
+const RoleContent = () => {
+    return <div className="vContent">
+        <p>{role}</p>
     </div>
 }
 
@@ -21,10 +28,13 @@ export const Floater = ({cat}:{cat: cats|null}) => {
         switch (cat) {
             case cats.goals:
                 return GoalsContent();
+            case cats.role: {
+                return RoleContent();
+            }
             default:
                 return null;
         }
-    }, [cat])
+    }, [cat]);
 
     if (cat === null){
         return null;
